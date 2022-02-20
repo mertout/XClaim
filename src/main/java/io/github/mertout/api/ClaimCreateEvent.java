80e73357e9cc294fded323ef61486d172ef6f3f1
mertout <86627790+mertout@users.noreply.github.com>
2022-02-20 19:28:41 +0300

    Update ClaimCreateEvent.java

--- src/main/java/io/github/mertout/api/ClaimCreateEvent.java
+++ src/main/java/io/github/mertout/api/ClaimCreateEvent.java
@@ -1,14 +1,22 @@
 package io.github.mertout.api;
 
+import io.github.mertout.core.data.DataHandler;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 public class ClaimCreateEvent extends Event implements Cancellable {
 
+    public Player p;
     public static final HandlerList handlers = new HandlerList();
     public boolean cancelled;
+
+    public ClaimCreateEvent(@NotNull Player x) {
+        this.p = x;
+    }
 
     @Override
     public boolean isCancelled() {
@@ -23,5 +31,9 @@ public class ClaimCreateEvent extends Event implements Cancellable {
     @Override
     public HandlerList getHandlers() {
         return handlers;
+    }
+
+    public Player getPlayer() {
+        return p;
     }
 }
