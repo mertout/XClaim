9e073b5ba46f9d86ea4f6728ed1afb8a9ff4e364
mertout <86627790+mertout@users.noreply.github.com>
2022-02-20 18:49:07 +0300

    Create BlockBreakListener.java

---
+++ src/main/java/io/github/mertout/listeners/BlockBreakListener.java
@@ -0,0 +1,29 @@
+package io.github.mertout.listeners;
+
+import io.github.mertout.Claim;
+import io.github.mertout.core.ClaimManager;
+import io.github.mertout.core.data.DataHandler;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class BlockBreakListener extends ClaimManager implements Listener {
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent e) {
+        if (Claim.getInstance().getClaimManager().hasClaimAtLoc(e.getBlock().getLocation(),e.getPlayer())) {
+            e.setCancelled(true);
+        }
+        else {
+            if (e.getBlock().getType() == Material.BEDROCK) {
+                DataHandler data = super.PlayerToClaim(e.getPlayer());
+                if (data != null) {
+                    if (data.getBlockLocation().equals(e.getBlock().getLocation())) {
+                        e.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+}
