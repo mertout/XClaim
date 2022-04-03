5a2fd795621bda3ee78de5d805b9611e80a74256
mertout <86627790+mertout@users.noreply.github.com>
2022-04-03 17:33:01 +0300

    Update XClaim.java

--- src/main/java/io/github/mertout/commands/XClaim.java
+++ src/main/java/io/github/mertout/commands/XClaim.java
@@ -1,7 +1,7 @@
 package io.github.mertout.commands;
 
 import io.github.mertout.Claim;
-import io.github.mertout.addons.XMaterial;
+import io.github.mertout.utils.XMaterial;
 import io.github.mertout.filemanager.MessagesFile;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -16,39 +16,38 @@ public class XClaim implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender cs, @NotNull Command cmd, @NotNull String s, @NotNull String[] strings) {
-        if (strings.length > 0) {
-            if (strings[0].equals("give")) {
-                if (cs instanceof Player) {
-                    Player p = (Player) cs;
-                    if (!p.hasPermission("xclaim.give")) {
-                        p.sendMessage(MessagesFile.convertString("messages.doesnt-permission"));
-                        return true;
-                    }
-                }
-                if (strings.length != 2) {
-                    cs.sendMessage( MessagesFile.convertString("messages.arg-error"));
-                    return true;
-                }
-                Player p2 = Bukkit.getPlayer(strings[1]);
-                ItemStack is = XMaterial.BEDROCK.parseItem();
-                ItemMeta im = is.getItemMeta();
-                im.setDisplayName(Claim.getInstance().getConfig().getString("settings.claim-block-name").replaceAll("&", "§"));
-                is.setItemMeta(im);
-                p2.getInventory().addItem(is);
-                return false;
-            }
-            else if (strings[0].equals("reload")) {
-                if (!cs.hasPermission("xclaim.reload")) {
-                    cs.sendMessage(MessagesFile.convertString("messages.doesnt-permission"));
-                    return true;
-                }
-                Claim.getInstance().reloadConfig();
-                MessagesFile.reloadMessages();
-                cs.sendMessage(MessagesFile.convertString("messages.reloaded-files"));
-            }
+        if (!(strings.length > 0)) {
+            cs.sendMessage( MessagesFile.convertString("messages.arg-error"));
+            return true;
         }
-        else {
-            cs.sendMessage(MessagesFile.convertString("messages.arg-error"));
+        if (strings[0].equals("give")) {
+            if (strings.length != 2) {
+                cs.sendMessage( MessagesFile.convertString("messages.arg-error"));
+                return true;
+            }
+            if (cs instanceof Player) {
+                Player p = (Player) cs;
+                if (!p.hasPermission("xclaim.give")) {
+                    p.sendMessage(MessagesFile.convertString("messages.doesnt-permission"));
+                    return true;
+                }
+            }
+            Player p2 = Bukkit.getPlayer(strings[1]);
+            ItemStack is = XMaterial.BEDROCK.parseItem();
+            ItemMeta im = is.getItemMeta();
+            im.setDisplayName(Claim.getInstance().getConfig().getString("settings.claim-block-name").replaceAll("&", "§"));
+            is.setItemMeta(im);
+            p2.getInventory().addItem(is);
+            return false;
+        }
+        else if (strings[0].equals("reload")) {
+            if (!cs.hasPermission("xclaim.reload")) {
+                cs.sendMessage(MessagesFile.convertString("messages.doesnt-permission"));
+                return true;
+            }
+            Claim.getInstance().reloadConfig();
+            MessagesFile.reloadMessages();
+            cs.sendMessage(MessagesFile.convertString("messages.reloaded-files"));
         }
         return false;
     }
