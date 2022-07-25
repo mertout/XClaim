cdf3940de4c6e92bffd3be9befeaa0faab92f68d
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:45 +0300

    Create MinecartEvent.java

---
+++ src/main/java/io/github/mertout/listeners/MinecartEvent.java
@@ -0,0 +1,17 @@
+package io.github.mertout.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import io.github.mertout.core.ClaimManager;
+
+public class MinecartEvent extends ClaimManager implements Listener
+{
+    @EventHandler
+    public void onMinecartEnter(final VehicleEnterEvent e) {
+        if (super.getChunk(e.getVehicle().getLocation()) != null && super.hasClaimAtLoc(e.getVehicle().getLocation(), (Player)e.getEntered())) {
+            e.setCancelled(true);
+        }
+    }
+}
