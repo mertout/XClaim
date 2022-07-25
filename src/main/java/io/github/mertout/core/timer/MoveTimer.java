93371b81ae07bac48723b8084c815466e041ae7f
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:09 +0300

    Update MoveTimer.java

--- src/main/java/io/github/mertout/core/timer/MoveTimer.java
+++ src/main/java/io/github/mertout/core/timer/MoveTimer.java
@@ -1,37 +1,42 @@
 package io.github.mertout.core.timer;
 
-import io.github.mertout.Claim;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.Bukkit;
+import io.github.mertout.Claim;
 import org.bukkit.entity.Player;
-
 import java.util.HashMap;
 
-public class MoveTimer implements Runnable {
-
-    private static final HashMap<Player, Integer> task = new HashMap<>();
-
-    public void addMoveCooldown(Player p) {
-        task.put(p,Claim.getInstance().getConfig().getInt("settings.move-block-cooldown"));
+public class MoveTimer implements Runnable
+{
+    private static final HashMap<Player, Integer> task;
+    
+    public void addMoveCooldown(final Player p) {
+        MoveTimer.task.put(p, Claim.getInstance().getConfig().getInt("settings.move-block-cooldown"));
     }
+    
     public MoveTimer() {
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(Claim.getInstance(), this, 0L, 20L);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask((Plugin)Claim.getInstance(), (Runnable)this, 0L, 20L);
     }
+    
     @Override
     public void run() {
-        if (!task.isEmpty()) {
-            if (Bukkit.getOnlinePlayers().size() > 0) {
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    int time = task.get(p) - 1;
-                    task.remove(p);
-                    task.put(p,time);
-                    if (time == 0) {
-                        task.remove(p);
-                    }
+        if (!MoveTimer.task.isEmpty() && Bukkit.getOnlinePlayers().size() > 0) {
+            for (final Player p : Bukkit.getOnlinePlayers()) {
+                final int time = MoveTimer.task.get(p) - 1;
+                MoveTimer.task.remove(p);
+                MoveTimer.task.put(p, time);
+                if (time == 0) {
+                    MoveTimer.task.remove(p);
                 }
             }
         }
     }
+    
     public static HashMap getMoveTask() {
-        return task;
+        return MoveTimer.task;
+    }
+    
+    static {
+        task = new HashMap<Player, Integer>();
     }
 }
