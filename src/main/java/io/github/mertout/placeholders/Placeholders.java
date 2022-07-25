b554877bf12bcddbb5b33a98e033c7eb6a10ef07
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:48 +0300

    Update Placeholders.java

--- src/main/java/io/github/mertout/placeholders/Placeholders.java
+++ src/main/java/io/github/mertout/placeholders/Placeholders.java
@@ -1,68 +1,70 @@
 package io.github.mertout.placeholders;
 
 import io.github.mertout.Claim;
-import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import io.github.mertout.core.ClaimManager;
+import io.github.mertout.filemanager.MessagesFile;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
-public class Placeholders extends PlaceholderExpansion {
-
-    public @NotNull String getAuthor() {
+public class Placeholders extends PlaceholderExpansion
+{
+    @NotNull
+    public String getAuthor() {
         return "mert.out";
     }
-
-    public @NotNull String getIdentifier() {
+    
+    @NotNull
+    public String getIdentifier() {
         return "xclaim";
     }
-
-    public @NotNull String getVersion() {
-        return "1.0.0";
+    
+    @NotNull
+    public String getVersion() {
+        return "1.0.2";
     }
-
+    
     public boolean persist() {
         return true;
     }
-
-    public String onPlaceholderRequest(Player p, @NotNull String identifier) {
-        switch(identifier) {
+    
+    public String onPlaceholderRequest(final Player p, @NotNull final String identifier) {
+        switch (identifier) {
             case "owner":
-                if (Claim.getInstance().getClaimManager().hasClaim(p)) {
-                    if (Claim.getInstance().getClaimManager().PlayerToClaim(p) != null) {
-                        return Claim.getInstance().getClaimManager().PlayerToClaim(p).getOwner();
-                    }
-                    else {
-                        return "";
-                    }
+                if (Claim.getInstance().getClaimManager().PlayerToClaim2(p) != null) {
+                    return Claim.getInstance().getClaimManager().PlayerToClaim2(p).getOwner();
                 }
-                else {
-                    return "";
+                return "";
+            case "team_size":
+                if (Claim.getInstance().getClaimManager().PlayerToClaim2(p) != null) {
+                    return Claim.getInstance().getClaimManager().PlayerToClaim2(p).getMembers().size() + "";
                 }
-            case "team-size":
-                if (Claim.getInstance().getClaimManager().hasClaim(p)) {
-                    if (Claim.getInstance().getClaimManager().PlayerToClaim(p) != null) {
-                        return Claim.getInstance().getClaimManager().PlayerToClaim(p).getMembers().size() + "";
-                    }
-                    else {
-                        return "";
-                    }
-                }
-                else {
-                    return "";
-                }
-            case "owner-location":
+                return "";
+            case "owner_via_location":
                 if (Claim.getInstance().getClaimManager().getChunk(p.getLocation()) != null) {
                     return Claim.getInstance().getClaimManager().getChunk(p.getLocation()).getOwner();
                 }
-                else {
-                    return "";
-                }
-            case "team-size-locationn":
+                return "";
+            case "team_size_via_location":
                 if (Claim.getInstance().getClaimManager().getChunk(p.getLocation()) != null) {
                     return Claim.getInstance().getClaimManager().getChunk(p.getLocation()).getMembers().size() + "";
                 }
-                else {
-                    return "";
+                return "";
+            case "remaining":
+                if (Claim.getInstance().getClaimManager().PlayerToClaim2(p) != null) {
+                    return ClaimManager.calcTime(Claim.getInstance().getClaimManager().PlayerToClaim2(p));
                 }
+                return "";
+            case "remaining_via_location":
+                if (Claim.getInstance().getClaimManager().getChunk(p.getLocation()) != null) {
+                    return ClaimManager.calcTime(Claim.getInstance().getClaimManager().getChunk(p.getLocation()));
+                }
+                return "";
+            case "ownership_via_location":
+                if (Claim.getInstance().getClaimManager().getChunk(p.getLocation()) != null) {
+                    return MessagesFile.convertString("messages.ownership.taken");
+                }
+                return MessagesFile.convertString("messages.ownership.untaken");
         }
         return null;
     }
