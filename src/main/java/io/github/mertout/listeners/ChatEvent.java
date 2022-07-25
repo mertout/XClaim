27df11531c652e146ed1389d397d35bc97fbba8c
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:31 +0300

    Update ChatEvent.java

--- src/main/java/io/github/mertout/listeners/ChatEvent.java
+++ src/main/java/io/github/mertout/listeners/ChatEvent.java
@@ -1,44 +1,41 @@
 package io.github.mertout.listeners;
 
-import io.github.mertout.Claim;
-import io.github.mertout.core.ClaimManager;
+import org.bukkit.event.EventHandler;
 import io.github.mertout.core.data.DataHandler;
+import org.bukkit.OfflinePlayer;
 import io.github.mertout.filemanager.MessagesFile;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import io.github.mertout.Claim;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.Listener;
+import io.github.mertout.core.ClaimManager;
 
-public class ChatEvent extends ClaimManager implements Listener {
-
+public class ChatEvent extends ClaimManager implements Listener
+{
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent e) {
-        if (Claim.getInstance().getMembers().contains(e.getPlayer())) {
-            if (super.hasClaim(e.getPlayer())) {
-                e.setCancelled(true);
-                if (!e.getMessage().equals("-")) {
-                    OfflinePlayer p = Bukkit.getServer().getOfflinePlayer(e.getMessage());
-                    if (p.isOnline()) {
-                        if (!e.getMessage().contains(e.getPlayer().getName())) {
-                            DataHandler data = super.PlayerToClaim(e.getPlayer());
-                            data.getMembers().add(e.getMessage());
-                            e.getPlayer().sendMessage(MessagesFile.convertString("messages.member-added")
-                                    .replace("{player}", e.getMessage()));
-                        }
-                        else {
-                            e.getPlayer().sendMessage(MessagesFile.convertString("messages.add-error"));
-                        }
+    public void onChat(final AsyncPlayerChatEvent e) {
+        if (Claim.getInstance().getMembers().contains(e.getPlayer()) && super.hasClaim(e.getPlayer())) {
+            e.setCancelled(true);
+            if (!e.getMessage().equals("-")) {
+                final OfflinePlayer p = Bukkit.getServer().getOfflinePlayer(e.getMessage());
+                if (p.isOnline()) {
+                    if (!e.getMessage().contains(e.getPlayer().getName())) {
+                        final DataHandler data = super.PlayerToClaim(e.getPlayer());
+                        data.getMembers().add(e.getMessage());
+                        e.getPlayer().sendMessage(MessagesFile.convertString("messages.member-added").replace("{player}", e.getMessage()));
                     }
                     else {
-                        e.getPlayer().sendMessage(MessagesFile.convertString("messages.player-not-found"));
+                        e.getPlayer().sendMessage(MessagesFile.convertString("messages.add-error"));
                     }
                 }
                 else {
-                    e.getPlayer().sendMessage(MessagesFile.convertString("messages.friends-add-canceled"));
+                    e.getPlayer().sendMessage(MessagesFile.convertString("messages.player-not-found"));
                 }
-                Claim.getInstance().getMembers().remove(e.getPlayer());
             }
+            else {
+                e.getPlayer().sendMessage(MessagesFile.convertString("messages.member-add-canceled"));
+            }
+            Claim.getInstance().getMembers().remove(e.getPlayer());
         }
     }
 }
