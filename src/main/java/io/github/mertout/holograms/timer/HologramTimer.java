9b9857cbea585737b0da301ef18aa08f9b491525
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:16 +0300

    Update HologramTimer.java

--- src/main/java/io/github/mertout/holograms/timer/HologramTimer.java
+++ src/main/java/io/github/mertout/holograms/timer/HologramTimer.java
@@ -1,14 +1,16 @@
 package io.github.mertout.holograms.timer;
 
-import io.github.mertout.Claim;
 import io.github.mertout.holograms.HologramCore;
+import org.bukkit.plugin.Plugin;
+import io.github.mertout.Claim;
 import org.bukkit.Bukkit;
 
-public class HologramTimer implements Runnable {
-
+public class HologramTimer implements Runnable
+{
     public HologramTimer() {
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(Claim.getInstance(), this, 0L, 20L);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(Claim.getInstance(), (Runnable)this, 0L, 20L);
     }
+    
     @Override
     public void run() {
         new HologramCore().updateHolograms();
