b995ba4eb6c0ea38c819f06719074d5a20e280ee
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:35:00 +0300

    Create ClaimDayRenewEvent.java

---
+++ src/main/java/io/github/mertout/api/events/ClaimDayRenewEvent.java
@@ -0,0 +1,46 @@
+package io.github.mertout.api.events;
+
+import io.github.mertout.core.data.DataHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class ClaimDayRenewEvent extends Event implements Cancellable {
+
+    public Player p;
+
+    public DataHandler data;
+    public static final HandlerList handlers;
+    public boolean cancelled;
+
+    public ClaimDayRenewEvent(@NotNull final Player p, @NotNull final DataHandler data) {
+        this.p = p;
+        this.data = data;
+    }
+
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    public void setCancelled(final boolean arg0) {
+        this.cancelled = arg0;
+    }
+
+    public HandlerList getHandlers() {
+        return ClaimDayRenewEvent.handlers;
+    }
+
+    public DataHandler getData() {
+        return this.data;
+    }
+
+    public Player getPlayer() {
+        return p;
+    }
+
+    static {
+        handlers = new HandlerList();
+    }
+}
