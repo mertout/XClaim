ff99d83dc7ea19bf232f0adf22eba36b0d2919c8
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:50 +0300

    Update HexColor.java

--- src/main/java/io/github/mertout/utils/HexColor.java
+++ src/main/java/io/github/mertout/utils/HexColor.java
@@ -1,28 +1,23 @@
 package io.github.mertout.utils;
 
+import java.util.regex.Matcher;
 import org.bukkit.ChatColor;
+import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class HexColor {
-
+public class HexColor
+{
     public static String hex(@NotNull String message) {
-        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
-        Matcher matcher = pattern.matcher(message);
-        while (matcher.find()) {
-            String hexCode = message.substring(matcher.start(), matcher.end());
-            String replaceSharp = hexCode.replace('#', 'x');
-
-            char[] ch = replaceSharp.toCharArray();
-            StringBuilder builder = new StringBuilder("");
-            for (char c : ch) {
+        final Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+        for (Matcher matcher = pattern.matcher(message); matcher.find(); matcher = pattern.matcher(message)) {
+            final String hexCode = message.substring(matcher.start(), matcher.end());
+            final String replaceSharp = hexCode.replace('#', 'x');
+            final char[] ch = replaceSharp.toCharArray();
+            final StringBuilder builder = new StringBuilder("");
+            for (final char c : ch) {
                 builder.append("&" + c);
             }
-
             message = message.replace(hexCode, builder.toString());
-            matcher = pattern.matcher(message);
         }
         return ChatColor.translateAlternateColorCodes('&', message);
     }
