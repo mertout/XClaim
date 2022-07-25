05bf15c2c97e12c136f3ec563ac5803f0b4060d5
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:04 +0300

    Update XClaim.java

--- src/main/java/io/github/mertout/commands/XClaim.java
+++ src/main/java/io/github/mertout/commands/XClaim.java
@@ -1,46 +1,29 @@
 package io.github.mertout.commands;
 
-import io.github.mertout.Claim;
-import io.github.mertout.utils.XMaterial;
-import io.github.mertout.filemanager.MessagesFile;
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import io.github.mertout.utils.HexColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.ItemStack;
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import io.github.mertout.Claim;
+import io.github.mertout.filemanager.MessagesFile;
+import org.bukkit.command.Command;
 import org.jetbrains.annotations.NotNull;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
 
 public class XClaim implements CommandExecutor {
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender cs, @NotNull Command cmd, @NotNull String s, @NotNull String[] strings) {
-        if (!(strings.length > 0)) {
-            cs.sendMessage( MessagesFile.convertString("messages.arg-error"));
+    public boolean onCommand(@NotNull final CommandSender cs, @NotNull final Command cmd, @NotNull final String s, @NotNull final String[] strings) {
+        if (strings.length <= 0) {
+            cs.sendMessage(MessagesFile.convertString("messages.arg-error"));
             return true;
-        }
-        if (strings[0].equals("give")) {
-            if (strings.length != 2) {
-                cs.sendMessage( MessagesFile.convertString("messages.arg-error"));
-                return true;
             }
-            if (cs instanceof Player) {
-                Player p = (Player) cs;
-                if (!p.hasPermission("xclaim.give")) {
-                    p.sendMessage(MessagesFile.convertString("messages.doesnt-permission"));
-                    return true;
-                }
-            }
-            Player p2 = Bukkit.getPlayer(strings[1]);
-            ItemStack is = XMaterial.BEDROCK.parseItem();
-            ItemMeta im = is.getItemMeta();
-            im.setDisplayName(Claim.getInstance().getConfig().getString("settings.claim-block-name").replaceAll("&", "§"));
-            is.setItemMeta(im);
-            p2.getInventory().addItem(is);
-            return false;
-        }
-        else if (strings[0].equals("reload")) {
+        if (strings[0].equals("reload")) {
             if (!cs.hasPermission("xclaim.reload")) {
                 cs.sendMessage(MessagesFile.convertString("messages.doesnt-permission"));
                 return true;
@@ -48,6 +31,42 @@ public class XClaim implements CommandExecutor {
             Claim.getInstance().reloadConfig();
             MessagesFile.reloadMessages();
             cs.sendMessage(MessagesFile.convertString("messages.reloaded-files"));
+            return false;
+        }
+        else if (strings[0].equals("give")) {
+            if (cs instanceof Player) {
+                final Player p = (Player)cs;
+                if (!p.hasPermission("xclaim.give")) {
+                    p.sendMessage(MessagesFile.convertString("messages.doesnt-permission"));
+                    return true;
+                }
+            }
+            if (strings.length != 2) {
+                cs.sendMessage(MessagesFile.convertString("messages.arg-error"));
+                return true;
+            }
+            if (Bukkit.getPlayer(strings[1]) == null) {
+                cs.sendMessage(MessagesFile.convertString("messages.player-is-not-online").replace("{player}", strings[1]));
+                return true;
+            }
+            final Player p2 = Bukkit.getPlayer(strings[1]);
+            ItemStack is = new ItemStack(Material.matchMaterial(Claim.getInstance().getConfig().getString("settings.claim-block.material")));
+            final ItemMeta im = is.getItemMeta();
+            im.setDisplayName(MessagesFile.convertStringCFG("settings.claim-block.display-name"));
+            final ArrayList<String> lore = new ArrayList<String>();
+            for (final String str : Claim.getInstance().getConfig().getStringList("settings.claim-block.lore")) {
+                lore.add(HexColor.hex(str).replaceAll("&", "§"));
+            }
+            im.setLore(lore);
+            is.setItemMeta(im);
+            NBTItem nbti = new NBTItem(is);
+            nbti.setBoolean("claimblock", true);
+            is = nbti.getItem();
+            p2.getInventory().addItem(is);
+            return false;
+        }
+        else {
+            cs.sendMessage(MessagesFile.convertString("messages.arg-error"));
         }
         return false;
     }
