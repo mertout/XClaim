224952b3834cd90cb12d983649d1915729ef75b2
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:38 +0300

    Update CommandEvent.java

--- src/main/java/io/github/mertout/listeners/CommandEvent.java
+++ src/main/java/io/github/mertout/listeners/CommandEvent.java
@@ -1,19 +1,19 @@
 package io.github.mertout.listeners;
 
-import io.github.mertout.Claim;
-import io.github.mertout.core.ClaimManager;
-import io.github.mertout.filemanager.MessagesFile;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import io.github.mertout.filemanager.MessagesFile;
+import io.github.mertout.Claim;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.Listener;
+import io.github.mertout.core.ClaimManager;
 
-public class CommandEvent extends ClaimManager implements Listener {
-
+public class CommandEvent extends ClaimManager implements Listener
+{
     @EventHandler
-    public void onCommand(PlayerCommandPreprocessEvent e) {
-        if (super.hasClaimAtLoc(e.getPlayer().getLocation(),e.getPlayer())) {
-            String cmd = e.getMessage().replace("/", "");
-            for (String str : Claim.getInstance().getConfig().getStringList("settings.disabled-commands")) {
+    public void onCommand(final PlayerCommandPreprocessEvent e) {
+        if (super.hasClaimAtLoc(e.getPlayer().getLocation(), e.getPlayer())) {
+            final String cmd = e.getMessage().replace("/", "");
+            for (final String str : Claim.getInstance().getConfig().getStringList("settings.disabled-commands")) {
                 if (cmd.equals(str)) {
                     e.getPlayer().sendMessage(MessagesFile.convertString("messages.disabled-commands-in-claim").replace("{command}", cmd));
                     e.setCancelled(true);
