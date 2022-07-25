c76271eaeb78e53ff4be437175d7a745078759a7
mert.out <86627790+mertout@users.noreply.github.com>
2022-07-26 02:37:11 +0300

    Update ClaimsFile.java

--- src/main/java/io/github/mertout/filemanager/ClaimsFile.java
+++ src/main/java/io/github/mertout/filemanager/ClaimsFile.java
@@ -1,43 +1,45 @@
 package io.github.mertout.filemanager;
 
-import io.github.mertout.Claim;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-
-import java.io.File;
 import java.io.IOException;
+import io.github.mertout.Claim;
+import java.io.File;
+import org.bukkit.configuration.file.FileConfiguration;
 
-public class ClaimsFile {
-
+public class ClaimsFile
+{
     public static FileConfiguration clfc;
     public static File clfile;
-
+    
     public static void loadClaimFiles() {
-        clfile = new File(Claim.getInstance().getDataFolder(), "claims.yml");
-        if (!clfile.exists()) {
+        ClaimsFile.clfile = new File(Claim.getInstance().getDataFolder(), "claims.yml");
+        if (!ClaimsFile.clfile.exists()) {
             try {
-                clfile.createNewFile();
-            } catch (IOException e) {
+                ClaimsFile.clfile.createNewFile();
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
-            clfc = YamlConfiguration.loadConfiguration(clfile);
-            clfc.createSection("claims");
+            (ClaimsFile.clfc = (FileConfiguration)YamlConfiguration.loadConfiguration(ClaimsFile.clfile)).createSection("claims");
             try {
-                clfc.save(clfile);
-            } catch (IOException e) {
+                ClaimsFile.clfc.save(ClaimsFile.clfile);
+            }
+            catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        clfc = YamlConfiguration.loadConfiguration(clfile);
-
+        ClaimsFile.clfc = (FileConfiguration)YamlConfiguration.loadConfiguration(ClaimsFile.clfile);
     }
+    
     public static FileConfiguration getClaimsFile() {
-        return clfc;
+        return ClaimsFile.clfc;
     }
+    
     public static void saveClaimsFile() {
         try {
-            clfc.save(clfile);
-        } catch (IOException e) {
+            ClaimsFile.clfc.save(ClaimsFile.clfile);
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
